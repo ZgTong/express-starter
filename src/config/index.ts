5245825e9c3e59534bd86720529ad09947a29880
phi1iptong <p.tong@clevertronics.com.au>
2026-02-03 11:09:36 +1100

    ts, env, srcript config

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+import { config } from 'dotenv';
+
+config({
+  path: `.env.${process.env.NODE_ENV || 'development'}.local`,
+});
+
+export const CREDENTIALS = process.env.CREDENTIALS === 'true';
+export const { NODE_ENV } = process.env; // 'development' | 'production' | 'test', injeceted by external tools, not in .env files
+export const { PORT,  ORIGIN } = process.env; // defined in .env files
